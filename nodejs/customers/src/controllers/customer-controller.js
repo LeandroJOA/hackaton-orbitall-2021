2b45c47f3a9cb6f1e31578184410502be166ce69
Leandro Ferreira Machado Capuano <leandrocapuano@gmail.com>
2021-01-20 15:57:13 -0300

    Commit inicial.

---
+++ nodejs/customers/src/controllers/customer-controller.js
@@ -0,0 +1,12 @@
+const neDB = require('../configurations/database')
+const api = {}
+
+api.findAll = (request, response) => {
+
+}
+
+api.getById = (request, response) => {
+
+}
+
+module.exports = api
