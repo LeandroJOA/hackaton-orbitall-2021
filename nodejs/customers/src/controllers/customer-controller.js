83c3cd3ff00940903d01eb3def0b21c0a8995666
Leandro J <leandrojoapi@gmail.com>
2021-01-21 22:02:29 -0300

    Implementação da API usando Node.js

--- nodejs/customers/src/controllers/customer-controller.js
+++ nodejs/customers/src/controllers/customer-controller.js
@@ -2,11 +2,50 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    
+    neDB.find({}).sort({name: 1}).exec((exception, customers) => {
+        if (exception) {
+            const setence = 'Opa, deu ruim na tentiva de listar todos os customers!'
+            console.log(setence, exception)
 
+            response.status(exception.status | 400)
+            response.json({'mensagem': setence})
+        }
+
+        response.status(226)
+        response.json(customers)
+    })
+    
+    /*
+    neDB.findOne({ name: 'Dio Branco'}).exec(function (exception, customers) {
+        if (exception) {
+            const setence = 'Opa, deu ruim na tentiva de listar todos os customers!'
+            console.log(setence, exception)
+
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+
+        response.status(226)
+        response.json(customers)
+    }) 
+    */
 }
 
-api.getById = (request, response) => {
+api.save = (request, response) => {
+    const canonical = request.body
 
+    neDB.insert(canonical, (exception, customer) => {
+        if (exception) {
+            console.log('Opa, deu ruim na tentiva de inserir um customer', exception)
+
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+
+        response.status(201)
+        response.json(customer)
+    })
 }
 
 module.exports = api
--- nodejs/customers/src/routes/customer-route.js
+++ nodejs/customers/src/routes/customer-route.js
@@ -3,6 +3,6 @@ const api = require('../controllers/customer-controller')
 module.exports = (app) => {
     app.route('/customers')
         .get(api.findAll)
-        .post(api.post)
+        .post(api.save)
 
 }
